c6eec4ba5b9cabd2a342042485e18eeaa67f3ee1
Anton Harnik <ajgarnik@gmail.com>
2025-12-19 15:44:16 +0200

    add extension

--- src/main/kotlin/net/agl/gradle/GitVersionPlugin.kt
+++ src/main/kotlin/net/agl/gradle/GitVersionPlugin.kt
@@ -4,8 +4,20 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import versionFromGit
 
+data class VersionFromGit(val projectVersion: String, val publishingVersion: String)
+
 class GitVersionPlugin : Plugin<Project> {
     override fun apply(project: Project) {
-        project.version = versionFromGit(project)
+        versionFromGit(project).also {
+            project.version = it
+            project.extensions.add(
+                "versionFromGit",
+                VersionFromGit(
+                    it,
+                    it.split("-", limit = 2)
+                        .let { v -> if (v.size == 2 && v[1] != "RELEASE") "${v[0]}-SNAPSHOT" else v[0] }
+                )
+            )
+        }
     }
 }
