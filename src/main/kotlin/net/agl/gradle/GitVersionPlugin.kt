16069498dcf9353eab6e7c30ecbdeac5bf027e37
Anton Harnik <ajgarnik@gmail.com>
2025-12-22 10:41:04 +0200

    code optimization

--- src/main/kotlin/net/agl/gradle/GitVersionPlugin.kt
+++ src/main/kotlin/net/agl/gradle/GitVersionPlugin.kt
@@ -4,20 +4,19 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import versionFromGit
 
-data class VersionFromGit(val projectVersion: String, val publishingVersion: String)
-
 class GitVersionPlugin : Plugin<Project> {
     override fun apply(project: Project) {
+
+        val ext = object {
+            val projectVersion: String by lazy { versionFromGit(project) }
+            val publishingVersion: String by lazy {
+                projectVersion.split("-", limit = 2)
+                    .let { v -> if (v.size == 2 && v[1] != "RELEASE") "${v[0]}-SNAPSHOT" else v[0] }
+            }
+        }
+
         versionFromGit(project).also {
-            project.version = it
-            project.extensions.add(
-                "versionFromGit",
-                VersionFromGit(
-                    it,
-                    it.split("-", limit = 2)
-                        .let { v -> if (v.size == 2 && v[1] != "RELEASE") "${v[0]}-SNAPSHOT" else v[0] }
-                )
-            )
+            project.extensions.add("versionFromGit", ext)
         }
     }
 }
